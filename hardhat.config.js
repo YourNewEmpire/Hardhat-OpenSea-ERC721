52d636f21afc9ac3da9f494e9927718c4f1935f2
Archie Smyth <archiesmyth26@googlemail.com>
2021-11-09 17:36:45 +0000

    delete api key on ps. clear from config

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,7 +4,7 @@ require("@nomiclabs/hardhat-etherscan");
 
 const privateKey = process.env.MNEMONIC
 const maticUrl = process.env.MATIC_APP_ID
-
+const polyScan = process.env.POLYGONSCAN
 module.exports = {
   solidity: "0.8.0",
   networks: {
@@ -17,6 +17,6 @@ module.exports = {
   //* Keep name as 'etherscan' to avoid errors.
   etherscan: {
     url: 'https://polygonscan.com/',
-    apiKey: 'M7MSTPDD1Y55184HA91S5TS4RCANBEPFBY'
+    apiKey: polyScan
   }
 };
